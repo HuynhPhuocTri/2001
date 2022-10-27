e33444af618d702f58493470c9de4c173ac674e6
HuynhPhuocTri <103299118+HuynhPhuocTri@users.noreply.github.com>
2022-10-27 19:46:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/tri.phuoc.3950';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = '“Anh biết hôm nay là một ngày đặc với em, với người con gái anh yêu. Anh muốn hôm nay sẽ trở nên đặc biệt hơn với em. Mừng sinh nhật em yêu, chúc em yêu thêm tuổi mới thêm nhiều yêu thương, luôn vui vẻ và thật hạnh phúc. Hãy luôn yêu anh nhiều như vậy nhé. cô gái bé nhỏ của anh”.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ present.js
@@ -0,0 +1,46 @@
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
